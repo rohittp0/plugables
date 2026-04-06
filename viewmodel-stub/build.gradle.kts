6f7408f27c3b8aae7b7e4a55172da5b43f50d266
Rohit T P <tprohit9@gmail.com>
2026-04-06 19:23:39 +0530

    chore: add viewmodel-stub subproject build config

---
+++ viewmodel-stub/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.rohittp.plugables"
+version = "1.0.0"
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+dependencies {
+    compileOnly("com.android.tools.build:gradle:8.5.0")
+    testImplementation(kotlin("test"))
+}
+
+gradlePlugin {
+    plugins {
+        create("viewModelStub") {
+            id = "com.rohittp.plugables.viewmodel-stub"
+            implementationClass = "com.rohittp.plugables.viewmodelstub.ViewModelStubPlugin"
+        }
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
